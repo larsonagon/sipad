418d4d67ca52a62b3c860a2289664ce12d559ebb
Larson Agon <larsonagon@Mac-mini-de-Larson.local>
2026-03-12 22:03:12 -0500

    fix postgres connection for render

--- backend/db/postgres.js
+++ backend/db/postgres.js
@@ -2,15 +2,55 @@ import pkg from 'pg'
 
 const { Pool } = pkg
 
-export const pool = new Pool({
-  user: 'larsonagon',
-  host: 'localhost',
-  database: 'sipad',
-  password: '',
-  port: 5432
-})
+// =======================================
+// CONFIG
+// =======================================
+
+const isProduction = process.env.DATABASE_URL !== undefined
+
+// =======================================
+// POOL
+// =======================================
+
+export const pool = isProduction
+
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    })
+
+  : new Pool({
+      user: 'larsonagon',
+      host: 'localhost',
+      database: 'sipad',
+      password: '',
+      port: 5432
+    })
+
+// =======================================
+// TEST CONNECTION
+// =======================================
+
+pool.connect()
+  .then(client => {
+    console.log('🐘 Conectado a PostgreSQL')
+
+    client.release()
+  })
+  .catch(err => {
+    console.error('❌ Error conectando a PostgreSQL:', err)
+  })
+
+// =======================================
+// QUERY HELPER
+// =======================================
 
 export async function query(text, params) {
+
   const res = await pool.query(text, params)
+
   return res
+
 }
