00a91342f8ac8c279a1b71d1eef72ee2c41558c4
Larson Agon <larsonagon@Mac-mini-de-Larson.local>
2026-03-24 20:19:38 -0500

    update

--- backend/modules/segtec/segtec.pdf.service.js
+++ backend/modules/segtec/segtec.pdf.service.js
@@ -15,7 +15,6 @@ export async function generarPDFActividad(actividad) {
     // 🔥 CONFIGURACIÓN CORRECTA LOCAL + RENDER
     browser = await puppeteer.launch({
       headless: 'new',
-      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
       args: [
         '--no-sandbox',
         '--disable-setuid-sandbox',
